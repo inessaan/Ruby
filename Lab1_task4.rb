4385f0e00f287b6602d9377f6e67b349373dc6f0
Inessa An <inessa.an004@mail.ru>
2022-03-15 20:36:03 +0300

    Lab1_task4_25

--- Lab1_task4.rb
+++ Lab1_task4.rb
@@ -2,11 +2,13 @@ def input
   return gets.chomp.split().map(&:to_i)
 end
 
+
 def get_file(path)
   file = File.new(path, "r:UTF-8")
   arr = file.read
   return arr.split.map(&:to_i)
 end
+
 
 def get_list()
   puts "Введите способ получения массива"
@@ -27,6 +29,7 @@ def get_list()
   end
 end
 
+
 # Задание 1
 
 def count_after_max(arr)
@@ -38,8 +41,33 @@ end
 
 # Задание 13
 
+
 def before_min(arr)
   return arr.push(arr.slice!(0...arr.index(arr.min)))
 end
 
-puts before_min(get_list())
+# Задание 25
+
+
+def max_in_interval(arr, a, b)
+  return arr[a...b].max
+end
+
+# Выбор задания
+
+puts "Выберите задание:
+1. - Задание 1
+2. - Задание 13
+3. - Задание 25"
+
+task = gets.chomp
+case task
+when '1'
+  puts count_after_max(get_list())
+when '2'
+  puts before_min(get_list())
+when '3'
+  print "Введите (a, b) через пробел: "
+  a, b = gets.chomp.split.map(&:to_i)
+  puts max_in_interval(get_list(), a, b)
+end
