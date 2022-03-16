03f8f07bfbb0f3f8c39e9bac6fef35c4a0885079
Inessa An <inessa.an004@mail.ru>
2022-03-16 08:48:07 +0300

    Lab1_task5_18

--- Lab1_task5.rb
+++ Lab1_task5.rb
@@ -8,3 +8,8 @@ puts str.count "А-я"
 str = "АабаА"
 str_palin = str.scan /А-я/
 puts str_palin == str_palin.reverse
+
+# задание 18
+
+str = "овалу 10.02.2002 авц10.03.2004 10.1.2005"
+puts str.scan(/([0-2][0-9]|3[0-1]).(0[1-9]|1[0-2]).([0-9]{4})/).map{|e| e.join('.')}.join(" ")
