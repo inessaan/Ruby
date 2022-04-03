258948ab648a2a35b15ecd93c8d53ad18375b5f2
Inessa An <inessa.an004@mail.ru>
2022-04-04 01:10:57 +0300

    lab1_task6

--- Lab1_task6.rb
+++ Lab1_task6.rb
@@ -5,17 +5,32 @@ puts str.scan(/([0-2][0-9]|3[0-1]).(0[1-9]|1[0-2]|января|февраля|м
 
 # задание 6.1
 
-puts "Введите строку:"
-str= STDIN.gets.chomp
-puts  str.scan(/\-\d+\.\d+|\d+\.\d+/).map{ |n| n.to_f }.max
+def max_float(str)
+  return str.scan(/\-\d+\.\d+|\d+\.\d+/).map{ |n| n.to_f }.max
+end
 
 # задание 6.9
-puts "Введите строку:"
-str= STDIN.gets.chomp
-puts str.scan(/[-+]?\d+\/\d+|[-+]?\d+\.\d+/).map{|e| e.to_f}.min
+
+def min_rat(str)
+  return str.scan(/[-+]?\d+\/\d+|[-+]?\d+\.\d+/).map{|e| e.to_f}.min
+end
 
 # задание 6.18
 
-puts "Введите строку:"
-str= STDIN.gets.chomp
-puts  str.scan(/\d+/).map{ |i| i.size }.max
+def max_count_dig(str)
+  return str.scan(/\d+/).map{ |i| i.size }.max
+end
+
+choice = ARGV[0]
+
+case choice
+when "1"
+  str = "34.0321, 189.4, 4.231 -1.23"
+  puts max_float(str)
+when "2"
+  str = "2/5 56.8 3/5 21.0 -10.9"
+  puts min_rat(str)
+when "3"
+  str="1234.p1 1234567g4"
+  puts max_count_dig(str)
+end
