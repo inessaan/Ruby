e54c48c3b1fac2e159de413a8d20e1622dfa687d
Inessa An <inessa.an004@mail.ru>
2022-04-04 01:57:38 +0300

    lab1_task7_1

--- Lab1_task7.rb
+++ Lab1_task7.rb
@@ -1,10 +1,23 @@
 file_name = ARGV[0]      #ruby lab1_task7.rb input.txt
 
-file = File.new(file_name, 'r:UTF-8')
-lines = file.readlines
-file.close
+f = File.new(file_name, 'r:UTF-8')
+str = f.readlines
+f.close
 
-puts lines.sort_by{ |i| i.size }
+puts str.sort_by{ |i| i.size }
 puts "*************"
-puts lines.sort_by{ |i| i.split().size }
+puts str.sort_by{ |i| i.split().size }
 
+puts "*************"
+
+def vowel_consonant (str)
+  return str.sort_by{|n| ((n.scan(/[аеёиоуэыюяАЕЁИОУЭЫЮЯ]/)).size - (n.scan(/[бвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЩ]/)).size)}
+end
+
+
+choice = ARGV[1]
+
+case choice
+when "1"
+  puts vowel_consonant(str)
+end
