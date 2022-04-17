61f48380f86693d5b1b49ff728f35c37c166bbae
Inessa An <inessa.an004@mail.ru>
2022-04-17 03:52:48 +0300

    Lab2_task1_2

--- Lab2/Lab2.rb
+++ Lab2/Lab2.rb
@@ -1,3 +1,15 @@
 class Department
+  def initialize (name, phone)
+    @name = name
+    @phone = phone
+  end
+
   attr_accessor :name, :phone
 end
+
+dep_rek = Department.new('Отдел подбора персонала', '89294723412')
+
+dep_it = Department.new('IT-отдел', '89265499854')
+
+puts "Подразделение: #{dep_rek.name}, контактный номер: #{dep_rek.phone}"
+puts "Подразделение: #{dep_it.name}, контактный номер: #{dep_it.phone}"
