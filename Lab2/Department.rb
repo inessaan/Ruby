cfd5c6c5a4234c631e51814caf38ba3a54a3c2b1
Inessa An <inessa.an004@mail.ru>
2022-05-25 02:21:22 +0300

    Исправила lab2_task1

--- Lab2/Department.rb
+++ Lab2/Department.rb
@@ -1,10 +1,25 @@
 class Department
   @duties = []
 
-  def initialize (name, phone, duties = nil)
-    @name = name
-    @phone = phone
+  def initialize (name,phone_number,duties=nil,selected_duty=nil)
+    self.name= name
+    self.phone= phone
     @duties = duties
+    @selected_duty = selected_duty
+
+  end
+
+
+  def phone=(phone)
+    if not Department.is_phone?(phone)
+      raise "Некорректный номер телефона"
+    end
+    phone.gsub!(/^8/, "7")
+    @phone = phone.delete("- )(+")
+  end
+
+  def self.is_phone?(phone)
+    return phone.scan(/^(\s*)?(\+?7|8)([(\- ]?\d{3}[)\- ]?)(\d{3}[\- ]?\d{2}[\- ]?\d{2}|\d{2}[\- ]?\d{3}[\- ]?\d{2})(\s*)?$/).length == 1
   end
 
   def name
@@ -16,52 +31,65 @@ class Department
   end
 
 
-  attr_accessor :name, :phone, :selected_duty
+  attr_accessor :name
+  attr_reader :phone
 
 
-  def add_duty(duty)
-    @duties.push(duty)
+  def Department.get_dep_str(dep)
+    dep = dep.split("\n")
+    dep_name,phone_number=dep[0].split("|")
+    duties=Hash.new
+    dep[1..dep.size].each do|duty|
+      duty,spec = duty.split(":")
+      duties[duty]=spec
+    end
+    Department.new(dep_name,phone_number,duties)
   end
 
-
-  def delete_duty(duty)
-    @duties.delete_at(@duties.index(duty))
+  # Добавить обязанность
+  def add_duty(duty,specification)
+    @duties[duty] = specification
   end
 
-
-  def show_duties
-    if @duties.empty?
-      "No duties were found in this department."
+  # Выделить обязанность
+  def select_duty(duty)
+    if @duties.has_key?(duty)
+      @select_duty = duty
     else
-      "Duties in this department: #{@duties.join(", ")}"
+      raise "Нет обязанности"
     end
   end
 
-
-  def select_duty(index)
-    @selected_duty = index
+  # Удалить обязанность
+  def delete_duty()
+    @duties.delete(@select_duty)
+    @select_duty = @duties.keys[0]
   end
 
-
-  def show_selected_duty
-    if @selected_duty.nil?
-      "Duty not selected"
-    else
-      "Selected duty: #{@duties[@selected_duty]}"
-    end
+  # Получить текст обязанности
+  def get_specification()
+    @duties[@select_duty]
   end
 
-  def replace_selected_duty(text)
-    @duties[@selected_duty] = text
-    "The selected duty has been renamed to #{ @duties[@selected_duty]}"
+  # Заменить текст выделенной обязанности
+  def change_specification(specification)
+    @duties[@select_duty] = specification
   end
+
+  # Получить обязанности
+  def get_duties()
+    @duties.keys.join(" | ")
+  end
+
 
   def to_s
-    unless @duties.nil?
-      duty = @duties.join(', ')
-      return "Department name: #{@name}\nContact phone: #{@phone}\nDuties: #{duty}\n\n"
-    end
-    "Department name: #{@name}\nContact phone: #{@phone}\n\n"
+    department_str="#{@name}|#{@phone}"
+    @duties.keys.each{|duty| department_str+="\n#{duty}:#{@duties[duty]}"}
+    department_str
+  end
+
+  def to_s_short()
+    department_str="#{@name}|#{@phone}"
   end
 
 end
--- Lab2/Main.rb
+++ Lab2/Main.rb
@@ -1,24 +1,84 @@
-current_path = File.dirname(__FILE__)
-require "#{current_path}/Department.rb"
+path = File.dirname(__FILE__) #путь к папке
+require "#{path}/Department.rb"
+require "#{path}/Department_list.rb"
 
-dep_rek = Department.new('Recruitment Department', '89294723412', ['aaa','bbb'])
+require "yaml"
 
-dep_it = Department.new('IT Department', '89265499854')
+# Считывание отделов из файла txt
+def read_from_txt(path)
+  arr = Array.new
+  file = File.new(path, "r:UTF-8")
+  text = file.read
+  arr = text.split((/\n_+\n/)).map do|dep|
+    Department.get_dep_str(dep)
+  end
+  arr
+end
+
+# Запись отделов в файл
+def write_to_txt(path, arr)
+  File.open("#{path}", "w") do |file|
+    arr.each do |e|
+      file.puts e
+      file.puts "_"
+    end
+  end
+end
+
+# Вывод отделов из файла
+def print_dep(arr)
+  arr.each do |dep|
+    puts dep
+    puts "_"
+  end
+end
+
+# Читаем ямл
+def read_from_yaml(path)
+  Psych.safe_load_file(path, permitted_classes: [Department])
+end
+
+# Записываем ямл
+def write_to_yaml(path, arr)
+  File.open(path, "w") do |file|
+    file.write(YAML.dump(arr))
+  end
+end
 
 
-puts dep_rek
-puts dep_rek.show_duties
 
+def main
 
-dep_rek.add_duty('qqq')
-puts dep_rek
+  #arr_dep=read_from_txt("Departments.txt")
+  #arr_dep=read_from_yaml("departments.yaml")
 
-dep_rek.delete_duty('qqq')
-puts dep_rek
+  #deps_list=Department_list.read_from_yaml("Departments.yaml")
 
-dep_rek.select_duty(1)
-puts dep_rek.show_selected_duty
+  #write_to_yaml("Departments.yaml",arr_dep)
+  #deps_list=Department_list.read_from_yaml("Departments.yaml")
+  #puts deps_list
+  #deps_list.add_note(Department.new("Отдел_Закупок","+89284355055"))
+  #deps_list.sort_by_name
+  #puts deps_list
 
-puts dep_rek.replace_selected_duty('ccc')
+  #dep=Department.new("Рекламный_отдел","+74234252230","Пиарить")
 
-puts dep_rek.show_duties
+  #dep.add_post("Гл_пиарщик","12","true")
+
+  deps_list=Department_list.read_from_yaml("Departments+Post.yaml")
+  #deps_list.add_note(dep)
+
+  #deps_list.choose_note(7)
+  #deps_list.delete_note()
+
+  puts deps_list.sort_by_numbers_vacancies
+
+  #puts deps_list.get_note.get_number_vacancies
+  #puts deps_list.to_s_short
+  deps_list.write_to_yaml("Departments+Post.yaml")
+end
+
+if __FILE__ == $0
+  main
+end
+
